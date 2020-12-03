b3b3dd9f4d95389302d3ceec80a238738e86b729
John Merigliano <jmerigliano@gmail.com>
2020-12-02 21:28:14 -0500

    new dev files

---
+++ src/data.js
@@ -0,0 +1,171 @@
+export const navData = [
+    {
+        name: "Projects",
+        url: "index.html#projects"
+    },
+    {
+        name: "LinkedIn",
+        url: "https://www.linkedin.com/in/johnmerigliano"
+    },
+    {
+        name: "GitHub",
+        url: "https://github.com/lautomator"
+    },
+    {
+        name: "Bitbucket",
+        url: "https://bitbucket.org/lautomator/"
+    }
+];
+
+export const siteData = {
+    name: "John Merigliano",
+    title: "Full Stack Web Developer",
+    description: "I am a multifaceted web applications developer with 15+ years experience in the industry. I continue to perfect my skills as a web programmer through ongoing education and engagement with open source web community. My analytical skills combined with persistence and motivation drive my problem-solving process. I enjoy collaborating with others and working as a team member to develop cogent solutions. The process of collaboration is important to me, as it enhances education and professional growth. The projects below are an example of the scope of my work.",
+    resume: "http://lautomator.github.io/apps/merigliano-resume-2020-12-no-addr.pdf",
+    footerText: "This site was created with React."
+};
+
+export const ProjectData = [
+    {
+        name: 'Calculator',
+        year: '2018',
+        description: 'This is a simple calculator. You can employ basic math functions. You can also add console output to memory to use later. The application demonstrates a Model View Controller (MVC) design pattern.',
+        repoURL: 'https://github.com/lautomator/calculator',
+        repoBadge: 'fa fa-git-square fa-2x',
+        projectURL: 'apps/calculator/',
+        projectImage: 'img/calc-cropped.png',
+        tech: [
+            'HTML5',
+            'CSS',
+            'JavaScript'
+        ]
+    },
+    {
+        name: 'Four Rooms',
+        year: '2018',
+        description: "Find your way out of these four rooms by picking up an using items to guide your way. Gain access to the telephone booth and teleport yourself from this dimension. A python",
+        repoURL: 'https://github.com/lautomator/four-rooms',
+        repoBadge: 'fa fa-git-square fa-2x',
+        projectURL: 'http://lautomator.github.io/apps/four-rooms',
+        projectImage: 'img/four-rooms.png',
+        tech: [
+            'HTML5',
+            'CSS',
+            'Javascript'
+        ]
+    },
+    {
+        name: 'Some Cards',
+        year: '2017',
+        description: 'I heard you wanted some cards. Well, here ya go! This simple web app prompts users to enter the number of cards they want dealt from a standard deck of 52 cards. The backend uses native javascript.',
+        repoURL: 'https://github.com/lautomator/some-cards/',
+        repoBadge: 'fa fa-git-square fa-2x',
+        projectURL: 'http://lautomator.github.io/apps/some-cards/',
+        projectImage: 'img/some-cards.png',
+        tech: [
+            'HTML5',
+            'CSS',
+            'Javascript'
+        ]
+    },
+    {
+        name: 'Simple Maze',
+        year: '2018',
+        description: "Get lost in a maze or find your way out by getting to the other side. This static maze's coordinates were generated from an image and a processor (built using Python code).",
+        repoURL: 'https://github.com/lautomator/simple-maze',
+        repoBadge: 'fa fa-git-square fa-2x',
+        projectURL: 'http://lautomator.github.io/apps/simple-maze',
+        projectImage: 'img/simple-maze.png',
+        tech: [
+            'HTML5',
+            'CSS',
+            'Javascript',
+            'Python'
+        ]
+    },
+    {
+        name: 'Codebreaker Game',
+        year: '2015',
+        description: 'This is a guessing game. This game is based on the classic Atari 2600 game of the same title. The object of the game is for the player to guess 3 numbers between 0 and 9 in the correct order. The game is written entirely in javaScript, HTML/CSS.',
+        repoURL: 'https://github.com/lautomator/codebreaker',
+        repoBadge: 'fa fa-git-square fa-2x',
+        projectURL: 'http://lautomator.github.io/apps/codebreaker/',
+        projectImage: 'img/codebreaker-1.png',
+        tech: [
+            'HTML5',
+            'CSS',
+            'Sass',
+            'JavaScript',
+            'MVC'
+        ]
+    },
+    {
+        name: 'Codebreaker Game (Command Line Version)',
+        year: '2014',
+        description: 'This is a guessing game. This game is based on the classic Atari 2600 game of the same title. The object of the game is for the player to guess n=level numbers between 0 and 9 in the correct order. The game is written entirely in Python.',
+        repoURL: 'https://bitbucket.org/lautomator/code-breaker/src/master/',
+        repoBadge: 'fa fa-git-square fa-2x',
+        projectURL: '',
+        projectImage: 'img/codebreaker-2.png',
+        tech: [
+            'Python'
+        ]
+    },
+    {
+        name: 'Frogger!',
+        year: '2015',
+        description: 'This project was created, in part, to fulfill the requirements of my Udacity Web Developer Nano Degree. It uses a game engine and object oriented programming.',
+        repoURL: 'https://github.com/lautomator/lautomator.github.io/tree/master/apps/frogger',
+        repoBadge: 'fa fa-git-square fa-2x',
+        projectURL: 'http://lautomator.github.io/apps/frogger',
+        projectImage: 'img/frogger.gif',
+        tech: [
+            'JavaScript',
+            'HTML5',
+            'CSS',
+            'jQuery'
+        ]
+    },
+    {
+        name: 'Image Controller MVC Demo',
+        year: '2016',
+        description: "This is an MVC design pattern demo using Knockout. The purpose of this implementation is to show how Knockout's observables by default create the separation of concerns between the object being acted upon and the logic behind it. Using Knockout is a good introduction into using the MVC design pattern.",
+        repoURL: 'https://github.com/lautomator/image-controller-mvc',
+        repoBadge: 'fa fa-git-square fa-2x',
+        projectURL: 'http://lautomator.github.io/apps/image-controller-mvc',
+        projectImage: 'img/image-mvc.png',
+        tech: [
+            'HTML5',
+            'CSS',
+            'jQuery',
+            'Knockout'
+        ]
+    },
+    {
+        name: 'Cycle Plus One',
+        year: '2016',
+        description: '[ALPHA: IN DEVELOPMENT] Cycle Plus One is a musical pattern generator (sequencer) that can be used to explore interesting sonic experiences. Using a fixed rhythmic profile, a steady eighth note pulse, you can experiment with tonality and density within a specified cycle of beats. The application can help composers create a matrix of tones that can be exported as MIDI or musical XML. From there, the material can be worked with further in a sequencing or music notation program. Cycle Plus One is meant to be a starting point for experimentation, allowing the composer to play with variation using a minimal amount of musical material.',
+        repoURL: 'https://sourceforge.net/p/cycle-plus-one/code/ci/master/tree/',
+        repoBadge: 'fa fa-git-square fa-2x',
+        projectURL: 'https://sourceforge.net/projects/cycle-plus-one/',
+        projectImage: 'img/cycle_plus_one_icon_alt.jpg',
+        tech: [
+            'Python',
+            'GUI'
+        ]
+    },
+    {
+        name: 'Line Processor',
+        year: '2018',
+        description: "Enter any amount of text and get some stats.",
+        repoURL: 'https://github.com/lautomator/line-processor',
+        repoBadge: 'fa fa-git-square fa-2x',
+        projectURL: 'http://lautomator.github.io/apps/line-processor',
+        projectImage: 'img/line-processor.png',
+        tech: [
+            'HTML5',
+            'CSS',
+            'Javascript'
+        ]
+    }
+];
