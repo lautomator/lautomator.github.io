cecc6551867c53ce55f15f309ac8362c14705b13
John Merigliano <jmerigliano@gmail.com>
2020-12-02 19:20:32 -0500

    Updates

---
+++ precache-manifest.13558e29b8f0931d0015a10549da1363.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2b77f3244102dca69fa032de1b0c8ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01799ad09ad076eb347e",
+    "url": "/static/css/main.460ecbf1.chunk.css"
+  },
+  {
+    "revision": "2d5576aab571ad31e219",
+    "url": "/static/js/2.2ccb126f.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/static/js/2.2ccb126f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01799ad09ad076eb347e",
+    "url": "/static/js/main.a3ab3509.chunk.js"
+  },
+  {
+    "revision": "69afeb986a2943e5f1f2",
+    "url": "/static/js/runtime-main.3c1a07a7.js"
+  }
+]);
